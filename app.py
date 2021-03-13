eccc2fa0234f998306308ac006dfd966f8a6d417
mohamedabcd <mo.abdennebi@gmail.com>
2021-03-13 20:19:17 +0000

    flask route

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, request, render_template, jsonify
-import logging
+from flask import Flask
+
 app = Flask(__name__)
 
-logging.basicConfig(filename='log.log', level=logging.DEBUG)
-
 @app.route('/')
-def index():
-    return "Hello World!"
+def hello():
+	return "Hello World!"
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True,port=3000)
