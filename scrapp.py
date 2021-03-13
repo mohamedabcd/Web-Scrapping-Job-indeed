1baabf73182804220742d254ca224f8cca03ac08
mohamedabcd <mo.abdennebi@gmail.com>
2021-03-13 21:02:25 +0000

    scrapp title-company-salary-link-city-id

--- scrapp.py
+++ scrapp.py
@@ -9,4 +9,38 @@ def extract(pages):
     r = requests.get(url)
 
     soup = BeautifulSoup(r.content, 'html.parser')
+    #a supp
+    transform(soup)
     return soup
+
+def transform(soup):
+    divs = soup.find_all('div', class_ ='jobsearch-SerpJobCard')
+    for item in divs:
+        title = item.find('a').text.strip()
+        compagny = item.find('span', class_ ='company').text.strip()
+
+        try:
+            salary = item.find('span', class_ = 'salaryText').text.strip()
+        except:
+            salary = '$'
+        summary = item.find('div', class_ = 'summary').text.strip().replace('\n','')
+        
+        lk = item.get('data-jk')
+        link = f'https://fr.indeed.com/voir-emploi?jk={lk}'
+        
+        city = item.get('data-rc-loc')
+
+        job = {
+            'title': title,
+            'company': compagny,
+            'salary': salary,
+            'summary': summary,
+            'link': link,
+            'city': city,
+            'idjob': lk
+        }
+        joblist.append(job)
+        
+    return joblist
+
+extract(0)
